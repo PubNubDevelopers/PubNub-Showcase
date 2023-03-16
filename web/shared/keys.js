ba09f1f494f298a3c121a0304f836a44d42680cb
Markus Kohler <markus.kohler@Markuss-MacBook-Pro.local>
2023-03-16 12:39:43 -0400

    Added cursor line to Geolocation

--- web/shared/keys.js
+++ web/shared/keys.js
@@ -3,6 +3,6 @@
  * Please see the ReadMe file for details of where to obtain keys
  */
 
-const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
-const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
-const geo_key = ''  //  ENTER YOUR GOOGLE API KEY HERE WITH THE FOLLOWING ENABLED: Maps JavaScript API, Geocoding API
+const publish_key = 'pub-c-fdf03b95-a47d-4e6f-a057-4d888e3ab8c8' //  ENTER YOUR PUBLISH KEY HERE
+const subscribe_key = 'sub-c-dea2387d-e6c9-4ccd-99c5-aaf8cb48d645' //  ENTER YOUR SUBSCRIBE KEY HERE
+const geo_key = 'AIzaSyDyl7ItKt5viBBju5Rwsqwrii5soyWUzp0';
